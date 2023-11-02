19e9b95ea50d6c46280655e526f11208b65de0ac
Ezra Natanael <ezranatanael2003@gmail.com>
2023-11-02 20:58:49 +0700

    fix: debugging bug GET articles

--- test/server_test.dart
+++ test/server_test.dart
@@ -3,7 +3,10 @@ import 'dart:io';
 import 'package:http/http.dart';
 import 'package:test/test.dart';
 
+import '../bin/article.dart';
+
 void main() {
+  List<Article> articles = [];
   final port = '8080';
   final host = 'http://localhost:$port';
 
@@ -29,11 +32,11 @@ void main() {
   test('POST articles', () async {
     final response = await post(
       Uri.parse(host + '/articles'),
-      body: '{"title" : "My super article", "content" : "My super content"}',
+      body: '{"title" : "HEY UMJI HERE!", "content" : "NICE TO MEET YOU"}',
     );
     expect(response.statusCode, 200);
     expect(response.body,
-        '{"title":"My super article","content":"My super content"}');
+        '{"title":"HEY UMJI HERE!","content":"NICE TO MEET YOU"}');
   });
 
   test('POST articles with empty string', () async {
@@ -44,12 +47,19 @@ void main() {
     expect(response.statusCode, 400);
   });
 
+  // DEBUGGING
   test('GET articles', () async {
     final response = await get(
-      Uri.parse(host + "/articles"),
+      Uri.parse(host + '/articles'),
     );
-    expect(response.statusCode, 200);
-    expect(response.body,
-        '[{"title":"My super article","content":"My super content"}]');
+
+    final articles = articlesFromJson(response.body);
+
+    // Expect that there is only one article.
+    expect(articles.length, 1);
+
+    // Expect that the article is correct.
+    expect(articles[0].title, 'HEY UMJI HERE!');
+    expect(articles[0].content, 'NICE TO MEET YOU');
   });
 }
